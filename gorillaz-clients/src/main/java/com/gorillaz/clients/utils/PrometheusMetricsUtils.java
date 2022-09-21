fb6e33eb40bb30288b0305a286366d68d62f3024
alexmuoso <alexmuosorio@gmail.com>
2022-09-21 12:15:38 -0500

    add metric , interceptor

---
+++ gorillaz-clients/src/main/java/com/gorillaz/clients/utils/PrometheusMetricsUtils.java
@@ -0,0 +1,31 @@
+package com.gorillaz.clients.utils;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import com.gorillaz.clients.config.MetricsConfig;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class PrometheusMetricsUtils {
+
+	private final MeterRegistry registry; 
+	
+	@Qualifier("requestCounter")
+	private final Counter requestCounter ;
+	
+	public PrometheusMetricsUtils(MeterRegistry registry, @Qualifier("requestCounter") Counter requestCounter) {
+		log.info("::::::::: METRICS - INITIALIZED ::::::: ");
+		this.registry = registry;
+		this.requestCounter = requestCounter;
+	}
+	
+	public void incrementRequest(String endpoint) {
+		log.info("::::::::: METRICS - REQUEST COUNTER INCREMENTED::::::: ");
+		this.registry.counter(MetricsConfig.REQUEST_COUNTER, MetricsConfig.ENDPOINT_TAG, endpoint).increment();
+	}
+}
